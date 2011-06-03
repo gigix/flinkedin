ecdc84b2e6535ec0fdd05245fcd74866f0d4c8c9
Jeff Xiong <gigix1980@gmail.com>
2011-06-03 15:59:59 +0800

    initial structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flinkedin::Application.config.secret_token = '74858005712e5415f22a4741d5c882e1eac343b711417b50ed155f0239a0ce1319ec4d1142df234c87ca4c443f8f30e0f217b8960ee9268a2366edac3370b52b'
